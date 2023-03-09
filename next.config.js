8f464c587640dd83056005f52230d36ecbbe395e
irswapio <pankajloniya@proton.me>
2023-03-09 18:14:37 +0000

    ref: disable sentry

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const { withSentryConfig } = require('@sentry/nextjs')
 const { withAxiom } = require('next-axiom')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: false,
+  enabled: process.env.ANALYZE === 'true',
 })
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin')
 const withTM = require('next-transpile-modules')(['@pancakeswap/uikit', '@pancakeswap/sdk'])
@@ -10,7 +10,7 @@ const withTM = require('next-transpile-modules')(['@pancakeswap/uikit', '@pancak
 const withVanillaExtract = createVanillaExtractPlugin()
 
 const sentryWebpackPluginOptions =
-  process.env.VERCEL_ENV !== 'production'
+  process.env.VERCEL_ENV === 'production'
     ? {
         // Additional config options for the Sentry Webpack plugin. Keep in mind that
         // the following options are set automatically, and overriding them is not
@@ -19,10 +19,13 @@ const sentryWebpackPluginOptions =
         //   urlPrefix, include, ignore
         silent: false, // Logging when deploying to check if there is any problem
         validate: true,
+        ignoreBuildErrors: true,
+        dryRun: !process.env.SENTRY_AUTH_TOKEN,
         // For all available options, see:
         // https://github.com/getsentry/sentry-webpack-plugin#options.
       }
     : {
+        ignoreBuildErrors: true,
         silent: true, // Suppresses all logs
         dryRun: !process.env.SENTRY_AUTH_TOKEN,
       }
