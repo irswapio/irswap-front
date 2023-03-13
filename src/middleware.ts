fe49fe734e4ff1d5d52bcda1b9db20f38522a928
irswapio <pankajloniya@proton.me>
2023-03-13 21:06:13 +0300

    ref: style

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-// Sanctioned Countries: Belarus, Cuba, Democratic Republic of Congo, Iran, Iraq, North Korea, Sudan, Syria, Zimbabwe.
-//const BLOCK_COUNTRIES = { BY: 'BY', CU: 'CU', CD: 'CD', IR: 'IR', IQ: 'IQ', KP: 'KP', SD: 'SD', SY: 'SY', ZW: 'ZW' }
+/* Sanctioned Countries: Belarus, Cuba, Democratic Republic of Congo, Iran, Iraq, North Korea, Sudan, Syria, Zimbabwe.
+const BLOCK_COUNTRIES = { BY: 'BY', CU: 'CU', CD: 'CD', IR: 'IR', IQ: 'IQ', KP: 'KP', SD: 'SD', SY: 'SY', ZW: 'ZW' }
 
 // Sanctioned Regions: Crimea
-//const BLOCK_REGIONS = { 'UA-43': 'UA-43' }
+const BLOCK_REGIONS = { 'UA-43': 'UA-43' }
+*/
 
 const BLOCK_COUNTRIES = {};
 const BLOCK_REGIONS = {};
