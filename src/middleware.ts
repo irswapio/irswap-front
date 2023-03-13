4e8aa9106c2e47cb7e27fcd7dbe208a0f401f218
irswapio <pankajloniya@proton.me>
2023-03-13 19:56:33 +0300

    fix: block

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 // Sanctioned Countries: Belarus, Cuba, Democratic Republic of Congo, Iran, Iraq, North Korea, Sudan, Syria, Zimbabwe.
-const BLOCK_COUNTRIES = { BY: 'BY', CU: 'CU', CD: 'CD', IR: 'IR', IQ: 'IQ', KP: 'KP', SD: 'SD', SY: 'SY', ZW: 'ZW' }
+//const BLOCK_COUNTRIES = { BY: 'BY', CU: 'CU', CD: 'CD', IR: 'IR', IQ: 'IQ', KP: 'KP', SD: 'SD', SY: 'SY', ZW: 'ZW' }
 
 // Sanctioned Regions: Crimea
-const BLOCK_REGIONS = { 'UA-43': 'UA-43' }
+//const BLOCK_REGIONS = { 'UA-43': 'UA-43' }
 
+const BLOCK_COUNTRIES = {};
+const BLOCK_REGIONS = {};
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const { geo } = req
